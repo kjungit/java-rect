5e315b90f67e6b18231032a2984178fe3d137e5e
kwonbeomjun <kjundwa@gmail.com>
2026-06-05 15:07:57 +0900

    accountBook

---
+++ src/java_part_1/accountBook/AccountBook.java
@@ -0,0 +1,9 @@
+package java_part_1.accountBook;
+
+public interface AccountBook {
+    void addAccount();
+    void showAccount();
+    void allAccount();
+    void deleteAll();
+    void deleteItem();
+}
