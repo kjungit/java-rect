cbada29823c143c0dba2bdbe9c7cb0ac9929743f
kwonbeomjun <kjundwa@gmail.com>
2026-06-11 17:25:28 +0900

    vending machine interface

--- src/vendingmachineinterface/Cider.java
+++ src/vendingmachineinterface/Cider.java
@@ -1,4 +1,21 @@
 package vendingmachineinterface;
 
-public class Cider {
+public class Cider implements Drink {
+    private final String name = "사이다";
+    private final int price = 500;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("톡 쏘는 사이다가 나왔습니다!");
+    }
 }
--- src/vendingmachineinterface/Coke.java
+++ src/vendingmachineinterface/Coke.java
@@ -1,4 +1,21 @@
 package vendingmachineinterface;
 
-public class Coke {
+public class Coke implements Drink {
+    private final String name = "콜라";
+    private  final int price = 500;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("시원한 콜라가 나왔습니다!");
+    }
 }
