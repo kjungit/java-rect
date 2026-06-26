79b75d953d364ee03d30c51f37850b0e895d77f0
kwonbeomjun <kjundwa@gmail.com>
2026-06-26 16:39:58 +0900

    strategy

--- spring-theory/src/main/java/spring/strategy/Database.java
+++ spring-theory/src/main/java/spring/strategy/Database.java
@@ -1,4 +1,20 @@
 package spring.strategy;
 
-public class Database {
+import java.util.ArrayList;
+import java.util.List;
+
+class Database {
+    private List<User> users = new ArrayList<>();
+
+    void open() {
+        System.out.println("  [컨텍스트] 연결 열기");
+    }
+
+    void close() {
+        System.out.println("  [컨텍스트] 연결 닫기");
+    }
+
+    List<User> getUsers() {
+        return users;
+    }
 }
--- spring-theory/src/main/java/spring/strategy/Main.java
+++ spring-theory/src/main/java/spring/strategy/Main.java
@@ -1,4 +1,21 @@
 package spring.strategy;
 
 public class Main {
+    public static void main(String[] args) {
+        Database db = new Database();
+        UserDao dao = new UserDao(db);
+
+        dao.deleteAll();
+        dao.add(new User("u1", "김"));
+        dao.add(new User("u2", "이"));
+
+        System.out.println("\n현재 사용자 수: " + db.getUsers().size());
+        for (User u : db.getUsers()) System.out.println("사용자: " + u.getName());
+
+
+        User user = dao.get("u1");
+
+        System.out.println(user.getName());
+    }
+
 }
