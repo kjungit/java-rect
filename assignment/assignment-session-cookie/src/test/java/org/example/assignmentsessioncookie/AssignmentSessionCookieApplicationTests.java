d3687e0dc3ba177e5347edde6fd1793352657771
kwonbeomjun <kjundwa@gmail.com>
2026-07-02 22:32:45 +0900

    init session cookie

---
+++ assignment/assignment-session-cookie/src/test/java/org/example/assignmentsessioncookie/AssignmentSessionCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assignmentsessioncookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSessionCookieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
